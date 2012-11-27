b663d993b6ef502f83d223987ad3e2f6ec51d10b
Bala <bala.iyer1973@gmail.com>
2012-11-27 10:50:42 +0000

    Added pagination with model

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 var SearchTwitter = {};
 
 SearchTwitter.Tweet = Backbone.Model.extend({
+    select: function () {
+        this.set({ selected: true });
+        SearchTwitter.vent.trigger('tweet:selected', this);
+    }
 });
 
 SearchTwitter.SearchTerm = Backbone.Model.extend({
@@ -13,31 +17,46 @@ SearchTwitter.TweetCollection = Backbone.Collection.extend({
 });
 
 SearchTwitter.vent = _.extend({}, Backbone.Events);
-	
+
 SearchTwitter.TweetListView = Backbone.View.extend({
-	template: '#tweet',
-	id: 'tweets',
-	initialize: function() {
-		this.template = $(this.template);
-		_.bindAll(this, 'renderTweet');
-		this.collection.bind('add', this.renderTweet, this);
-		this.collection.bind('reset', this.clearTweets, this);
-	},
-	
-	renderTweet: function(tweet) {
+    template: '#tweet',
+    id: 'tweets',
+    initialize: function () {
+        this.template = $(this.template);
+        _.bindAll(this, 'renderTweet');
+        this.collection.bind('add', this.renderTweet, this);
+        this.collection.bind('reset', this.clearTweets, this);
+    },
+
+    renderTweet: function (tweet) {
 		var html = _.template(this.template.html(), tweet.toJSON());
 		$(this.el).append(html);
-	},
-	
-	render: function() {
-		this.clearTweets();
-		this.collection.each(this.renderTweet);
-	},
-	
-	clearTweets: function() {
-		$(this.el).html('');
-	}
+    },
+
+    render: function () {
+        this.clearTweets();
+        this.collection.each(this.renderTweet);
+    },
+
+    clearTweets: function () {
+        $(this.el).html('');
+    }
 });
+
+SearchTwitter.Pagination = Backbone.Model.extend({
+    defaults: { 
+        nextPage: ''
+    }
+});
+
+SearchTwitter.Pager = Backbone.View.extend({
+    template: '#pagination',
+    render: function () {
+        var html = _.template($(this.template).html(), this.model.toJSON());
+        $(this.el).append(html);
+    }
+})
+
 	
 SearchTwitter.SearchTwitterView = Backbone.View.extend({
 	template: '#search-twitter-form',
@@ -84,28 +103,35 @@ SearchTwitter.Router = Backbone.Router.extend({
     }
 });
 
-(function($) {
+(function ($) {
     var searchTerm = new SearchTwitter.SearchTerm();
-	var tweets = new SearchTwitter.TweetCollection();
-	var searchTwitter = new SearchTwitter.SearchTwitterView({ model: searchTerm });
-	searchTwitter.render();
-	
-	var tweetListView = new SearchTwitter.TweetListView({collection: tweets});
-	
-	SearchTwitter.vent.bind('tweetsFound', function(data, searchTerm) {
-		var tweetsFound = new SearchTwitter.TweetCollection();
-		router.navigate("searchTwitter/" + searchTerm);
-		for (var tweet in data.results) {
-			tweetsFound.add(data.results[tweet]);
-		} 
-		tweetListView.collection = tweetsFound;
-		tweetListView.render();
-		$(tweetListView.el).appendTo($('div.container'));
-	});
-	
-	$(searchTwitter.el).appendTo($('div.container'));
-	
-	var router = new SearchTwitter.Router();
-	
-	Backbone.history.start();
+    var tweets = new SearchTwitter.TweetCollection();
+    var searchTwitter = new SearchTwitter.SearchTwitterView({ model: searchTerm });
+    searchTwitter.render();
+
+    var tweetListView = new SearchTwitter.TweetListView({ collection: tweets });
+
+    SearchTwitter.vent.bind('tweetsFound', function (data, searchTerm) {
+        var tweetsFound = new SearchTwitter.TweetCollection();
+        router.navigate("searchTwitter/" + searchTerm);
+        for (var tweet in data.results) {
+            tweetsFound.add(data.results[tweet]);
+        }
+        tweetListView.collection = tweetsFound;
+        tweetListView.render();
+        $(tweetListView.el).appendTo($('div.container'));
+
+        if (data.next_page) {
+            var pagination = new SearchTwitter.Pagination({nextPage: data.next_page});
+            var pager = new SearchTwitter.Pager({ model : pagination});
+            pager.render();
+            $(pager.el).appendTo($('div.container'));
+        }
+    });
+
+    $(searchTwitter.el).appendTo($('div.container'));
+
+    var router = new SearchTwitter.Router();
+
+    Backbone.history.start();
 })(jQuery);
